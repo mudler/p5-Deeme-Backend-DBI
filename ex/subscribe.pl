0efd59a31d1fae6df3c8aec4f527fd433306273a
mudler <mudler@dark-lab.net>
2014-08-12 18:17:08 +0200

    fixing examples

--- ex/subscribe.pl
+++ ex/subscribe.pl
@@ -1,23 +1,27 @@
 use Deeme;
 use strict;
-use Deeme::Backend::Mango;
+use Deeme::Backend::DBI;
 
 use feature 'say';
 
 my $Deeme = Deeme->new(
     backend => Deeme::Backend::DBI->new(
-        database => ["dbi:SQLite:dbname=/var/tmp/deeme.db"]  )
+        database => "dbi:SQLite:dbname=/var/tmp/deeme.db"
+    )
 );
 
-
-$Deeme->once(roar => sub {
-  my ($tiger, $times) = @_;
-  say 'RAWR! , You should see me only once' for 1 .. $times;
-});
-$Deeme->on(roar => sub {
-  my ($tiger, $times) = @_;
-  say 'You can see me for 3 times i guess' for 1 .. $times;
-});
+$Deeme->once(
+    roar => sub {
+        my ( $tiger, $times ) = @_;
+        say 'RAWR! , You should see me only once' for 1 .. $times;
+    }
+);
+$Deeme->on(
+    roar => sub {
+        my ( $tiger, $times ) = @_;
+        say 'You can see me for 3 times i guess' for 1 .. $times;
+    }
+);
 
 say "Events added";
 
